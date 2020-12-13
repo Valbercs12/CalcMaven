cc9648890b32fe1d555206af9f00fe3bb1f96a32
Valbercs12 <71413082+Valbercs12@users.noreply.github.com>
2020-12-13 17:53:03 -0300

    Create Operator.java

---
+++ src/main/java/valber/calculator/Operator.java
@@ -0,0 +1,13 @@
+package valber.calculator;
+
+public enum Operator {
+
+    ADD("ADD"), SUB("SUB"), MUL("MUL"), DIV("DIV"), ERR("ERR");
+    private String strName;
+
+    private Operator(String strName) {
+        this.strName = strName;
+    }
+
+
+}
